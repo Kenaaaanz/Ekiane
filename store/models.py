26197bf67ade4631c04a5dcc416750dd70abe42f
kenaaaanz <gichabakenani@gmail.com>
2026-04-16 21:00:18 +0300

    COMMIT 6: CLOUDINARY-STORAGE V

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.urls import reverse
 from cloudinary.models import CloudinaryField
+from django.urls import reverse
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -19,7 +20,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     cost_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    image = models.CloudinaryField('image')
+    image = CloudinaryField('image')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     stock = models.PositiveIntegerField(default=0)
     available = models.BooleanField(default=True)
