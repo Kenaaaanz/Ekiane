cc7117ef08fe18e8a8c4835557294e0010cf2ba0
kenaaaanz <gichabakenani@gmail.com>
2026-05-18 09:27:52 +0300

    COMMIT 21: GOOGLE ANALYTICS & GOOGLE TAG MANAGER INTEGRATION

--- store/context_processors.py
+++ store/context_processors.py
@@ -5,3 +5,10 @@ def currency(request):
     return {
         'currency_symbol': getattr(settings, 'CURRENCY_SYMBOL', 'KES'),
     }
+
+def google_analytics(request):
+    from django.conf import settings
+    return {
+        'GTM_ID': getattr(settings, 'GOOGLE_TAG_MANAGER_ID', ''),
+        'GA_ID': getattr(settings, 'GOOGLE_ANALYTICS_ID', ''),
+    }
